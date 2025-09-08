2e5286466aec093cdf138e021b404be3ffccad57
Matthew Heino <61652462+HeinoPortfolio@users.noreply.github.com>
2025-09-08 20:44:15 +0000

    chore: created basic interface components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//import { Post } from './components/Post.jsx'
+import { PostList } from './components/PostList'
 
-function App() {
-  const [count, setCount] = useState(0)
+// Create a list of posts
+const posts = [
+  {
+    title: 'Full-Stack Reac Projects',
+    contents: "Let's become full-stack developers.",
+    author: 'Daniel Bugl',
+  },
+  { title: 'Hello React!' },
+]
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+/*
+export function App(){
+	return (
+		<Post
+			title='Full-Stack React Projects'
+			contents="Let's become full-stack developers"
+			author= 'Daniel Bugl'
+		/>
+	)
 }
-
-export default App
+*/
+export function App() {
+  return <PostList posts={posts} />
+}
