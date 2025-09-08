d467f030be957bfa39d537e1a8a0063bf07ae087
Matthew Heino <61652462+HeinoPortfolio@users.noreply.github.com>
2025-09-08 23:14:42 +0000

    chore: created the remaining interface components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 //import { Post } from './components/Post.jsx'
 import { PostList } from './components/PostList'
+import { CreatePost } from './components/CreatePost'
+import { PostFilter } from './components/PostFilter'
+import { PostSorting } from './components/PostSorting'
 
-// Create a list of posts
+// Create a list of posts====================================================
 const posts = [
   {
     title: 'Full-Stack Reac Projects',
@@ -11,17 +14,19 @@ const posts = [
   { title: 'Hello React!' },
 ]
 
-/*
-export function App(){
-	return (
-		<Post
-			title='Full-Stack React Projects'
-			contents="Let's become full-stack developers"
-			author= 'Daniel Bugl'
-		/>
-	)
-}
-*/
+// Show all the components for the page =====================================
 export function App() {
-  return <PostList posts={posts} />
+  return (
+    <div style={{ padding: 8 }}>
+      <CreatePost />
+      <br />
+      <hr />
+      Filter By:
+      <PostFilter field='author' />
+      <br />
+      <PostSorting fields={['createdAt', 'updatedAt']} />
+      <hr />
+      <PostList posts={posts} />
+    </div>
+  )
 }
