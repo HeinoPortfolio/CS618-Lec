fb2c0956a696d31e165baa6b13b13fff823e8334
Matthew Heino <61652462+HeinoPortfolio@users.noreply.github.com>
2025-09-08 14:58:30 +0000

    chore: installed cors and tested it

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import { postsRoutes } from './routes/posts.js'
+import cors from 'cors'
 
 const app = express()
+app.use(bodyParser.json())
+app.use(cors())
 
 postsRoutes(app)
-app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send('Hello from Express!')
