acbdd28bc9c3db939d890df4f56d94e29edb7b04
Matthew Heino <61652462+HeinoPortfolio@users.noreply.github.com>
2025-09-09 14:37:00 +0000

    chore: tanstack setup completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,12 @@
-//import { Post } from './components/Post.jsx'
-import { PostList } from './components/PostList'
-import { CreatePost } from './components/CreatePost'
-import { PostFilter } from './components/PostFilter'
-import { PostSorting } from './components/PostSorting'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Blog } from './Blog.jsx'
 
-// Create a list of posts====================================================
-const posts = [
-  {
-    title: 'Full-Stack Reac Projects',
-    contents: "Let's become full-stack developers.",
-    author: 'Daniel Bugl',
-  },
-  { title: 'Hello React!' },
-]
+const queryClient = new QueryClient()
 
-// Show all the components for the page =====================================
 export function App() {
   return (
-    <div style={{ padding: 8 }}>
-      <CreatePost />
-      <br />
-      <hr />
-      Filter By:
-      <PostFilter field='author' />
-      <br />
-      <PostSorting fields={['createdAt', 'updatedAt']} />
-      <hr />
-      <PostList posts={posts} />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Blog />
+    </QueryClientProvider>
   )
 }
