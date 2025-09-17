788ccdac22422566824d8278ab3dfd805f6e27c9
Matthew Heino <61652462+HeinoPortfolio@users.noreply.github.com>
2025-09-17 09:45:14 +0000

    chore:  fetching blog posts

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -3,12 +3,22 @@ import { CreatePost } from './components/CreatePost.jsx'
 import { PostFilter } from './components/PostFilter.jsx'
 import { PostSorting } from './components/PostSorting.jsx'
 
+import { useQuery } from '@tanstack/react-query'
+import { getPosts } from './api/posts.js'
+/*
 const posts = [
   { title: 'full', contents: 'somestuff', author: 'me' },
   { title: 'sometitle' },
 ]
-
+*/
 export function Blog() {
+  const postsQuery = useQuery({
+    queryKey: ['posts'],
+    queryFn: () => getPosts(),
+  })
+
+  const posts = postsQuery.data ?? []
+
   return (
     <div style={{ padding: 8 }}>
       <CreatePost />
