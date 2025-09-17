1938b9fdaa6dd3a63caa48116c660bc602c732eb
Matthew Heino <61652462+HeinoPortfolio@users.noreply.github.com>
2025-09-17 18:09:14 +0000

    feat: added creating a post functionality

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,7 +1,19 @@
+// API function to get posts ============================================
 export const getPosts = async (queryParams) => {
   const res = await fetch(
     `${import.meta.env.VITE_BACKEND_URL}/posts?` +
       new URLSearchParams(queryParams),
   )
+  return await res.json()
+}
+
+// API function to create posts ========================================
+export const createPost = async (post) => {
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/posts`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(post),
+  })
+
   return await res.json()
 }
