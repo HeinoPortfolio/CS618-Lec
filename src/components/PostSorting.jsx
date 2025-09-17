8e6e99e7112924df25150abc66e79be00d6703a1
Matthew Heino <61652462+HeinoPortfolio@users.noreply.github.com>
2025-09-17 11:21:07 +0000

    chore: setup filtering and sorting

--- src/components/PostSorting.jsx
+++ src/components/PostSorting.jsx
@@ -2,6 +2,10 @@ import PropTypes from 'prop-types'
 
 export function PostSorting({
   fields = [],
+  value,
+  onChange,
+  orderValue,
+  onOrderChange,
 }) {
   return (
     <div>
@@ -9,6 +13,8 @@ export function PostSorting({
       <select
         name='sortBy'
         id='sortBy'
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
       >
         {fields.map((field) => (
           <option key={field} value={field}>
@@ -21,6 +27,8 @@ export function PostSorting({
       <select
         name='sortOrder'
         id='sortOrder'
+        value={orderValue}
+        onChange={(e) => onOrderChange(e.target.value)}
       >
         <option value={'ascending'}>ascending</option>
         <option value={'descending'}>descending</option>
@@ -31,4 +39,8 @@ export function PostSorting({
 
 PostSorting.propTypes = {
   fields: PropTypes.arrayOf(PropTypes.string).isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  orderValue: PropTypes.string.isRequired,
+  onOrderChange: PropTypes.func.isRequired,
 }
